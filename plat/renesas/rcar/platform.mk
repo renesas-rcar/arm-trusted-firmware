96f10ef60049479269e6107dae7e39ac6a1c7e2a
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2020-11-30 20:29:53 +0900

    rcar_gen3: plat: BL31: Add SiP Service
    Added SiP Service provides the following data via SMC
     - Version
     - Number of SiP Service commands
     - UID
     - Board type
    
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>

--- plat/renesas/rcar/platform.mk
+++ plat/renesas/rcar/platform.mk
@@ -422,6 +422,8 @@ BL31_SOURCES	+=	${RCAR_GIC_SOURCES}				\
 			plat/renesas/rcar/aarch64/platform_common.c	\
 			plat/renesas/rcar/bl31_plat_setup.c		\
 			plat/renesas/rcar/plat_pm.c			\
+			plat/renesas/rcar/rcar_sip_svc.c	\
+			drivers/renesas/rcar/board/board.c		\
 			drivers/renesas/rcar/console/rcar_console.S	\
 			drivers/renesas/rcar/console/rcar_printf.c	\
 			drivers/renesas/rcar/delay/micro_delay.c	\
