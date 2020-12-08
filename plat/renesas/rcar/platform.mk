05b5cb2f4c7ba2ce1bd86338929e1e9633fe8255
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2020-12-08 16:27:29 +0900

    rcar_gen3: plat: BL31:　Disable stack protector
    This commit disables the BL31 stack protector because the canary value
    in the DRAM cannot be read when running Suspend To RAM.
    
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>

--- plat/renesas/rcar/platform.mk
+++ plat/renesas/rcar/platform.mk
@@ -380,7 +380,8 @@ PLAT_BL_COMMON_SOURCES	:=	drivers/renesas/rcar/iic_dvfs/iic_dvfs.c \
 				plat/renesas/rcar/rcar_common.c
 
 ifneq (${ENABLE_STACK_PROTECTOR},0)
-PLAT_BL_COMMON_SOURCES	+=	plat/renesas/rcar/rcar_stack_protector.c
+BL_COMMON_SOURCES	+=	plat/renesas/rcar/rcar_stack_protector.c
+BL31_CFLAGS	+=	-fno-stack-protector
 endif
 
 RCAR_GIC_SOURCES	:=	drivers/arm/gic/common/gic_common.c	\
