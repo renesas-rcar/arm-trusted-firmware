d72e22560258c25d3f8f6316442bcb8d882c58be
Takumi Noguchi <takumi.noguchi.kq@hitachi.com>
2021-02-22 10:18:45 +0900

    Add from_bl2_to_bl31_parameter in bl31_entrypoint

---
+++ include/bl31/bl31_entrypoint.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __BL31_ENTRYPOINT_H__
+#define __BL31_ENTRYPOINT_H__
+
+
+
+#endif /* __BL31_ENTRYPOINT_H__ */
