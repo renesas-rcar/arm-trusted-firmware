ae68c121e423b3148eec419455fd4ee61e0a6f54
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2021-07-12 18:34:38 +0900

    rcar_gen3: plat: BL31: Add a check not to stop a boot CPU
    This commit adds a check so that the boot CPU does not stop to
    execute self-refresh.
    
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>
    Signed-off-by: Hieu Nguyen <hieu.nguyen.dn@renesas.com>

--- plat/renesas/common/bl31_plat_setup.c
+++ plat/renesas/common/bl31_plat_setup.c
@@ -131,3 +131,17 @@ void bl31_platform_setup(void)
 	rcar_boot_mpidr = read_mpidr_el1() & 0x0000ffffU;
 	rcar_pwrc_all_disable_interrupt_wakeup();
 }
+
+uint32_t bl31_plat_boot_mpidr_chk(void)
+{
+	uint32_t rc = RCAR_MPIDRCHK_NOT_BOOTCPU;
+	uint64_t tmp_mpidr;
+
+	tmp_mpidr = read_mpidr_el1() & 0x0000ffffU;
+
+	if (tmp_mpidr == rcar_boot_mpidr) {
+		rc = RCAR_MPIDRCHK_BOOTCPU;
+	}
+	return rc;
+}
+
