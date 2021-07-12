6b945bfc4c15884d300f2aeeee6bd106b03dfa68
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2021-07-12 19:36:53 +0900

    rcar_gen3: plat: BL31: Remove SiP Service
    Remove (revert) SiP Service, because the getting board-type function
    is no longer needed.
    The any functionality provided by the SiP service be nothing when
    the getting board-type is removed.
    
    Signed-off-by: Koichi Yamaguchi <koichi.yamaguchi.zb@hitachi.com>
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
    Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>
    Signed-off-by: Hieu Nguyen <hieu.nguyen.dn@renesas.com>

--- plat/renesas/rcar/platform.mk
+++ plat/renesas/rcar/platform.mk
@@ -323,9 +323,7 @@ PLAT_INCLUDES	+=	-Idrivers/renesas/common/ddr		\
 			-Idrivers/renesas/common/scif		\
 			-Idrivers/renesas/common/emmc		\
 			-Idrivers/renesas/common/pwrc		\
-			-Idrivers/renesas/common/io			\
-			-Iplat/renesas/rcar/include			\
-			-Iplat/renesas/rcar
+			-Idrivers/renesas/common/io
 
 
 BL2_SOURCES	+=	plat/renesas/rcar/bl2_plat_setup.c	\
@@ -341,8 +339,6 @@ endif
 ifneq (${ENABLE_STACK_PROTECTOR},0)
 BL_COMMON_SOURCES	+=	plat/renesas/rcar/rcar_stack_protector.c
 endif
-BL31_SOURCES	+=	plat/renesas/rcar/rcar_sip_svc.c	\
-			drivers/renesas/rcar/board/board.c
 
 ifeq (${RCAR_GEN3_ULCB},1)
 BL31_SOURCES		+=	drivers/renesas/rcar/cpld/ulcb_cpld.c
