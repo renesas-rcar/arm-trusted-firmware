renesas-rcar/arm-trusted-firmware

commit 65755155eec06df8850ad94c5cd33ac38a5d2d8d

rcar_gen3: plat: Add DRAM size judgment by PRR register
This commit adds an argument to bl2_advertise_dram_size because the
M3 DRAM size is can only be determined by the cut version.

Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>

plat/renesas/rcar/bl2_plat_setup.c
@@ -614,6 +614,7 @@ static void bl2_advertise_dram_size(uint32_t product)
 		[4] = 0x600000000ULL,
 		[6] = 0x700000000ULL,
 	};
+	uint32_t cut = mmio_read_32(RCAR_PRR) & PRR_CUT_MASK;
 
 	switch (product) {
 	case PRR_PRODUCT_H3:
@@ -639,15 +640,24 @@ static void bl2_advertise_dram_size(uint32_t product)
 		break;
 
 	case PRR_PRODUCT_M3:
+		if (cut < PRR_PRODUCT_30) {
 #if (RCAR_GEN3_ULCB == 1)
-		/* 2GB(1GBx2 2ch split) */
-		dram_config[1] = 0x40000000ULL;
-		dram_config[5] = 0x40000000ULL;
+			/* 2GB(1GBx2 2ch split) */
+			dram_config[1] = 0x40000000ULL;
+			dram_config[2] = 0x600000000ULL;
+			dram_config[3] = 0x40000000ULL;
 #else
-		/* 4GB(2GBx2 2ch split) */
-		dram_config[1] = 0x80000000ULL;
-		dram_config[5] = 0x80000000ULL;
+			/* 4GB(2GBx2 2ch split) */
+			dram_config[1] = 0x80000000ULL;
+			dram_config[2] = 0x600000000ULL;
+			dram_config[3] = 0x80000000ULL;
 #endif
+		} else {
+			/* 8GB(2GBx4 2ch split) */
+			dram_config[1] = 0x100000000ULL;
+			dram_config[2] = 0x600000000ULL;
+			dram_config[3] = 0x100000000ULL;
+		}
 		break;
 
 	case PRR_PRODUCT_M3N:

commit bf221415f1e57729c55fa57851c0e51f7eac9ec1

rcar_gen3: plat: Fix DRAM size judgment by PRR register
The even dram_config[] array fields contain DRAM bank base addresses,
while odd dram_config[] array fields contain DRAM bank size. Fix the
assignment for M3W.

2 GiB M3W has 1 GiB DRAM at bank 0x4_0000_0000 and 1 GiB at 0x6_0000_0000,
4 GiB M3W has 2 GiB DRAM at bank 0x4_0000_0000 and 2 GiB at 0x6_0000_0000.

Fixes: 65755155e ("rcar_gen3: plat: Add DRAM size judgment by PRR register")
Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
Change-Id: Id2938d1d8ee4a81d6bca7a977eae3d6174c3fdb4

plat/renesas/rcar/bl2_plat_setup.c
@@ -641,19 +641,16 @@ static void bl2_advertise_dram_size(uint32_t product)
 #if (RCAR_GEN3_ULCB == 1)
 			/* 2GB(1GBx2 2ch split) */
 			dram_config[1] = 0x40000000ULL;
-			dram_config[2] = 0x600000000ULL;
-			dram_config[3] = 0x40000000ULL;
+			dram_config[5] = 0x40000000ULL;
 #else
 			/* 4GB(2GBx2 2ch split) */
 			dram_config[1] = 0x80000000ULL;
-			dram_config[2] = 0x600000000ULL;
-			dram_config[3] = 0x80000000ULL;
+			dram_config[5] = 0x80000000ULL;
 #endif
 		} else {
 			/* 8GB(2GBx4 2ch split) */
 			dram_config[1] = 0x100000000ULL;
-			dram_config[2] = 0x600000000ULL;
-			dram_config[3] = 0x100000000ULL;
+			dram_config[5] = 0x100000000ULL;
 		}
 		break;
 