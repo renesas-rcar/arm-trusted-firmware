b5ad4738d907ce3e98586b453362db767b86f45d
Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
2023-04-23 01:01:22 +0900

    rcar-gen4: BL31: change scmi-sys-suspend and self-refresh sequence
    DDR self refresh during communication between G4MH/SCP and CR52.
    Therefore, the order was changed.
    
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>

--- drivers/renesas/rcar_gen4/pwrc/pwrc.c
+++ drivers/renesas/rcar_gen4/pwrc/pwrc.c
@@ -433,8 +433,8 @@ static void __section(".system_ram") rcar_pwrc_set_self_refresh(void)
 static void __section(".system_ram") __attribute__ ((noinline))
 		rcar_pwrc_go_suspend_to_ram(void)
 {
-	rcar_pwrc_set_self_refresh();
 	rcar_scmi_sys_suspend();
+	rcar_pwrc_set_self_refresh();
 
 	wfi();
 
