afd834bcf765a099de93ca1758b7ad9314f026cc
Hieu Nguyen <hieu.nguyen.dn@renesas.com>
2024-08-12 15:44:44 +0700

    rcar-gen4: plat: BL31: Fix build error when upgrade to v2.11
    Signed-off-by: Hieu Nguyen <hieu.nguyen.dn@renesas.com>

--- plat/renesas/rcar_gen4/platform.mk
+++ plat/renesas/rcar_gen4/platform.mk
@@ -104,6 +104,8 @@ BL31_SOURCES	+=	${RCAR_GIC_SOURCES}				\
 			plat/renesas/rcar_gen4/plat_pm.c		\
 			plat/renesas/rcar_gen4/plat_pm_scmi.c		\
 			plat/renesas/rcar_gen4/rcar_common.c		\
+			drivers/delay_timer/delay_timer.c       \
+			drivers/delay_timer/generic_delay_timer.c   \
 			drivers/renesas/rcar_gen4/pwrc/call_sram.S	\
 			drivers/renesas/rcar_gen4/pwrc/pwrc.c		\
 			drivers/renesas/rcar_gen4/scif/scif.c		\
